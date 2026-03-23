687701d02758e5ec0e0cdeedd4e697b2dbc66c27
Patrick D. Taylor <patrickdtaylor1980@gmail.com>
2026-03-23 19:56:10 -0400

    edited userRoutes

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -8,7 +8,7 @@ const bcrypt = require('bcrypt');
 router.post("/register", async (req, res) => {
   try {
     const { username, email, password } = req.body;
-    
+
     const product = await Product.create(req.body);
     res.status(201).json(product);
   } catch (error) {
@@ -24,4 +24,6 @@ router.post("/login", async (req, res) => {
     res.status(400).json({ message: error.message });
   }
 });
+
+module.exports = router;
 
