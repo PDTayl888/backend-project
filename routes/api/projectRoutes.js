7a5e05a0cc91890a3d2335892718e3b7ed3d2e9c
Patrick D. Taylor <patrickdtaylor1980@gmail.com>
2026-03-24 20:03:25 -0400

    edited projectRoutes

--- routes/api/projectRoutes.js
+++ routes/api/projectRoutes.js
@@ -1,8 +1,42 @@
 const express = require("express");
 const router = express.Router();
-const Product = require("../models/Product");
+const Project = require("../models/Project");
+const authMiddleWare = require("../../utils/authMiddleware");
 
-router.post("/", async (req, res) => {
+router.post("/", authMiddleWare, async (req, res) => {
+  try {
+    const project = await Project.create({
+      ...req.body,
+      user: req.user.id,
+    });
+    res.status(201).json(project);
+  } catch (error) {
+    res.status(500).json({ message: 'SERVER ERRRO', error.message });
+  }
+});
+
+router.get("/", authMiddleWare,async (req, res) => {
+  try {
+    const projects = await Project.find({ user: req.user.id });
+    res.status(200).json(projects);
+  } catch (error) {
+    res.status(500).json({ message: "SERVER ERROR", error: error.message });
+  }
+});
+
+router.get("/:id", authMiddleWare, async (req, res) => {
+  try {
+    const project = await Project.findOne({ _id: req.params.id, user: req.user.id });
+    if (!project) {
+      return res.status(403).json({ message: "PROJECT NOT FOUND"});
+    }
+    res.status(200).json(project);
+  } catch (error) {
+    res.status(500).json({ message: 'SERVER ERROR', error: error.message });
+  }
+});
+
+router.put("/:id", authMiddleWare, async (req, res) => {
   try {
     const { username, email, password } = req.body;
 
@@ -13,7 +47,8 @@ router.post("/", async (req, res) => {
   }
 });
 
-router.post("/", async (req, res) => {
+
+router.delete("/:id", authMiddleWare, async (req, res) => {
   try {
     const { username, email, password } = req.body;
 
@@ -23,6 +58,5 @@ router.post("/", async (req, res) => {
     res.status(400).json({ message: error.message });
   }
 });
-
 
 module.exports = router;
