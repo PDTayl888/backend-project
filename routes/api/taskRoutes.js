d5abb6d6bded17a2a40394746b2b61f5d2729671
Patrick D. Taylor <patrickdtaylor1980@gmail.com>
2026-03-24 21:36:04 -0400

    edited taskRoutes

--- routes/api/taskRoutes.js
+++ routes/api/taskRoutes.js
@@ -25,4 +25,56 @@ router.post("/projects/:projectId/tasks", async (req, res) => {
   }
 });
 
+router.get("/projects/:projectId/tasks", async (req, res) => {
+  try {
+    const project = await Project.findById(req.params.projectId);
+    
+    if (!project) {
+      return res.status(404).json({ message: "PROJECT NOT FOUND" });
+    }
+    if (project.user.toString() !== req.user.id) {
+      return res.status(403).json({ message: "NOT YOURS BUDDY" });
+    }
+
+    const tasks = await Task.find({ project: req.params.projectId });
+    res.status(200).json(tasks);
+  } catch (error) {
+    res.status(500).json({ message: "SERVER ERROR", error: error.message });
+  }
+});
+
+router.put("/tasks/:taskId", async (req, res) => {
+  try {
+    const task = await Task.findById(req.params.taskId).populate('project');
+
+    if (!task) {
+      return res.status(404).json({ message: "TASK NOT FOUND" });
+    }
+    if (task.project.user.toString() !== req.user.id) {
+      return res.status(403).json({ message: "NOT YOURS BUDDY" });
+    }
+
+    const updatedTask = await Task.findByIdAndUpdate(req.params.taskId, req.body, { new: true });
+    res.status(200).json(updatedTask);
+  } catch (error) {
+    res.status(500).json({ message: "SERVER ERROR", error: error.message });
+  }
+});
+
+router.delete("/tasks/:taskId", async (req, res) => {
+  try {
+    const task = await Task.findById(req.params.taskId).populate('project');
+    if (!task) {
+      return res.status(404).json({ message: "TASK NOT FOUND" });
+    }
+    if (task.project.user.toString() !== req.user.id) {
+      return res.status(403).json({ message: "NOT YOURS BUDDY" });
+    }
+    await Task.findByIdAndDelete(req.params.taskId);
+    res.status(200).json({ message: "TASK SUCCESSFULLY DELETED" });
+  } catch (error) {
+    res.status(500).json({ message: "SERVER ERROR", error: error.message });
+  }
+});
+
 module.exports = router;
